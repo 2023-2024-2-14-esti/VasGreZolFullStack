d12dfb8bd2c83e0a5af2452a820f5f290c5f4473
VasGreZol <bsze3grezol@vasvari.org>
2025-02-10 00:47:30 +0100

    FullStackContext

---
+++ VasGreZolFullStack/Backend/Context/FullStackContext.cs
@@ -0,0 +1,18 @@
+﻿using VasGreZolFullStack.Common.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace VasGreZolFullStack.Backend.Context
+{
+    public class FullStackContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        public FullStackContext(DbContextOptions<FullStackContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
