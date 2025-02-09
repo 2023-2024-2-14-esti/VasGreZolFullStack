083f6d70a38155d4747555419d6ec04f07c14d43
VasGreZol <bsze3grezol@vasvari.org>
2025-02-09 20:47:32 +0100

    Kiindulási alap

---
+++ VasGreZolFullStack/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VasGreZolFullStack
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
