18fc127e2a70f741086dcc28ef1904524a8eb7e3
VasGreZol <bsze3grezol@vasvari.org>
2025-02-10 00:50:11 +0100

    FullStackInMemoryContext

---
+++ VasGreZolFullStack/Backend/Context/FullStackInMemoryContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VasGreZolFullStack.Backend.Context
+{
+    public class FullStackInMemoryContext : FullStackContext
+    {
+        public FullStackInMemoryContext(DbContextOptions<FullStackContext> options) : base(options) { }
+    }
+}
