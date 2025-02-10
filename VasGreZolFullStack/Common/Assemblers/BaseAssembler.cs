3068130078e4b51bf3dc04d446d05c5c2df9e8ef
VasGreZol <bsze3grezol@vasvari.org>
2025-02-10 02:50:10 +0100

    BaseAssembler

---
+++ VasGreZolFullStack/Common/Assemblers/BaseAssembler.cs
@@ -0,0 +1,8 @@
+﻿namespace VasGreZolFullStack.Common.Assemblers
+{
+    public abstract class BaseAssembler<TModel, TDto>
+    {
+        public abstract TModel ToModel(TDto dto);
+        public abstract TDto ToDto(TModel domainEntity);
+    }
+}
