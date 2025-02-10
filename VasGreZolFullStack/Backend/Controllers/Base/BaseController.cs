06d6477ea198c56f57a9c0a49df47bd944f778d3
VasGreZol <bsze3grezol@vasvari.org>
2025-02-10 02:44:56 +0100

    CarTransform

--- VasGreZolFullStack/Backend/Controllers/Base/BaseController.cs
+++ VasGreZolFullStack/Backend/Controllers/Base/BaseController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using VasGreZolFullStack.Backend.Repos;
+
 
 namespace VasGreZolFullStack.Backend.Controllers.Base
 {
---
+++ VasGreZolFullStack/Common/Transforms/CarTransform.cs
@@ -0,0 +1,35 @@
+﻿using VasGreZolFullStack.Common.Dtos;
+using VasGreZolFullStack.Common.Models;
+
+namespace VasGreZolFullStack.Common.Transforms
+{
+    public static class CarTransform
+    {
+        public static Dtos.CarDto ToCarDto(this Car car)
+        {
+            return new Dtos.CarDto
+            {
+                Id = car.Id,
+                Brand = car.Brand,
+                Name = car.Name,
+                YearOut = car.YearOut,
+                PowerLe = car.PowerLe,
+                EngineSize = car.EngineSize,
+                Color = car.Color
+            };
+        }
+        public static Models.Car ToCar(this CarDto carDto)
+        {
+            return new Models.Car
+            {
+                Id = carDto.Id,
+                Brand = carDto.Brand,
+                Name = carDto.Name,
+                YearOut = carDto.YearOut,
+                PowerLe = carDto.PowerLe,
+                EngineSize = carDto.EngineSize,
+                Color = carDto.Color
+            };
+        }
+    }
+}
