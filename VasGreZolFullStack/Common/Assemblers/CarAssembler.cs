286305111cd0f3b1bee830fc3a1ec33e802cc7e1
VasGreZol <bsze3grezol@vasvari.org>
2025-02-10 02:56:56 +0100

    CarAssembler

---
+++ VasGreZolFullStack/Common/Assemblers/CarAssembler.cs
@@ -0,0 +1,18 @@
+﻿using VasGreZolFullStack.Common.Dtos;
+using VasGreZolFullStack.Common.Models;
+using VasGreZolFullStack.Common.Transforms;
+
+namespace VasGreZolFullStack.Common.Assemblers
+{
+    public class CarAssembler : BaseAssembler<Car, CarDto>
+    {
+        public override CarDto ToDto(Car domainEntity)
+        {
+            return domainEntity.ToCarDto();
+        }
+        public override Car ToModel(CarDto dto)
+        {
+            return dto.ToCar();
+        }
+    }
+}
