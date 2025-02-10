dbc44749415ebc0312b5098597f98ba86fd0e1a3
VasGreZol <bsze3grezol@vasvari.org>
2025-02-10 01:39:41 +0100

    IQuerybleExtension

---
+++ VasGreZolFullStack/Backend/Repos/Base/IQuerybleExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using VasGreZolFullStack.Common.Models;
+
+namespace VasGreZolFullStack.Backend.Repos.Base
+{
+    public static class IQuerybleExtension
+    {
+        public static IQueryable<TEntity> FindByCondition<TEntity>(this IQueryable<TEntity> query, Expression<Func<TEntity, bool>> expression)
+            where TEntity : class, IDbEntity<TEntity>, new()
+        {
+            return query.Where(expression);
+        }
+    }
+}
