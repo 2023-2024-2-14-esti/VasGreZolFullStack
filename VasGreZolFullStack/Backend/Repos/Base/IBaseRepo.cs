cf30b241d4f3d002b3340e733186e15a87a7ac0b
VasGreZol <bsze3grezol@vasvari.org>
2025-02-10 01:11:44 +0100

    IBaseRepo

--- VasGreZolFullStack/Backend/Repos/Base/IBaseRepo.cs
+++ VasGreZolFullStack/Backend/Repos/Base/IBaseRepo.cs
@@ -1,9 +1,15 @@
 ﻿using VasGreZolFullStack.Common.Models;
-
+using VasGreZolFullStack.Common.Responses;
+using System.Linq.Expressions;
 
 namespace VasGreZolFullStack.Backend.Repos.Base
 {
-    internal interface IBaseRepo
+    public interface IBaseRepo <TEntity> where TEntity : class, IDbEntity<TEntity>, new()
     {
+        public Task<IEnumerable<TEntity>> GetAllAsync();
+        public Task<IEnumerable<TEntity>> FindByConditionAsync(Expression<Func<TEntity, bool>> expression);
+        public Task<Response> UpdateAsync(TEntity entity);
+        public Task<Response> CreateAsync(TEntity entity);
+        public Task<Response> DeleteAsync(Guid id);
     }
 }
