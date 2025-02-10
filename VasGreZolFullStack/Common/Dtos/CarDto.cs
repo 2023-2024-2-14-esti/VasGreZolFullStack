f9d0eb8f79da3737d7d48e74093bbb734c5e1eb6
VasGreZol <bsze3grezol@vasvari.org>
2025-02-10 02:32:08 +0100

    CarDto

---
+++ VasGreZolFullStack/Common/Dtos/CarDto.cs
@@ -0,0 +1,34 @@
+﻿namespace VasGreZolFullStack.Common.Dtos
+{
+    public class CarDto
+    {
+        public Guid Id { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public int YearOut { get; set; }
+        public int PowerLe { get; set; }
+        public int EngineSize { get; set; }
+        public string Color { get; set; }
+
+        public CarDto(Guid id, string brand, string name, int year, int powerLe, int engineSize, string color)
+        {
+            Id = id;
+            Brand = brand;
+            Name = name;
+            YearOut = year;
+            PowerLe = powerLe;
+            EngineSize = engineSize;
+            Color = color;
+        }
+        public CarDto()
+        {
+            Id = Guid.NewGuid();
+            Brand = string.Empty;
+            Name = string.Empty;
+            YearOut = 0;
+            PowerLe = 0;
+            EngineSize = 0;
+            Color = string.Empty;
+        }
+    }
+}
